7d5fd867b602e14e6f5435b73c6f28a0bcb43f89
Ayush Jaiswal <ayush45aj@gmail.com>
2025-02-13 07:13:19 +0530

    Update README.md

--- client/tailwind.config.js
+++ client/tailwind.config.js
@@ -1,3 +1,7 @@
+import { createRequire } from 'module';
+
+const require = createRequire(import.meta.url);
+
 /** @type {import('tailwindcss').Config} */
 export default {
     darkMode: ["class"],
@@ -54,5 +58,5 @@ export default {
   	}
   },
   plugins: [require("tailwindcss-animate")],
-}
+};
 
